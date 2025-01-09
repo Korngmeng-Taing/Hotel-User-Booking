b8861c60165446539d241bb5dbcbe6b3913fe4f3
Taing korngmeng <korngmeng015@gmail.com>
2025-01-09 10:19:35 +0700

    fix

--- Hotel_booking/bookings/decoration.py
+++ Hotel_booking/bookings/decoration.py
@@ -9,17 +9,3 @@ def unauthenticated_user(view_func):
         else:
             return view_func(request,*args,**kwargs)
     return wrapper_func
-def allowed_user(allowed_roles=[]):
-    def decorator(view_func):
-        def wrapper_func(request, *args, **kwargs):
-            group=None
-            if request.user.groups.exists():
-                group=request.user.groups.all()[0].name
-            if group in allowed_roles:
-                return view_func(request, *args, **kwargs)
-            else:
-                return HttpResponse('You are not allow to view this page')
-            return view_func(request, *args, **kwargs)
-        return wrapper_func
-    return decorator
-    
