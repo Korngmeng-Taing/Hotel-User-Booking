4922fe1757a6670139f03a4a98b85b1c30e5c010
Taing korngmeng <korngmeng015@gmail.com>
2025-01-15 23:06:04 +0700

    update

--- Hotel_booking/bookings/decoration.py
+++ Hotel_booking/bookings/decoration.py
@@ -9,3 +9,14 @@ def unauthenticated_user(view_func):
         else:
             return view_func(request,*args,**kwargs)
     return wrapper_func
+
+def admin_only(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if request.user.is_staff:
+                return view_func(request, *args, **kwargs)
+            else:
+                return redirect('dashboard')
+        else:
+            return redirect('login')
+    return wrapper_func
